8c467b6bd9b85e8e5560daf87aabaf5f92a8b5b2
Joseph Ahn <joseph.t.ahn@gmail.com>
2015-02-06 16:40:04 -0500

    implement linked list

--- graph.js
+++ graph.js
@@ -50,10 +50,40 @@ var LinkedList = function() {
   this.tail = null;
 };
 
+// adds to tail
 LinkedList.prototype.add = function(node) {
-
+  if (this.head === null) {
+    this.head = node;
+    this.tail = node;
+  } else {
+    this.tail.next = node;
+    this.tail = node;
+  }
 };
 
 LinkedList.prototype.remove = function(node) {
+  var curr = this.head;
+  // for single length linked list
+  if (curr !== null && curr.next === null) {
+    this.head = null;
+    this.tail = null;
+    return;
+  }
 
+  // head is node to be removed
+  if (curr.val === node.val) {
+    this.head = this.head.next;
+    return;
+  }
+
+  while(curr.next !== null) {
+    if (curr.next.val === node.val) {
+      curr.next = curr.next.next;
+      if (curr.next === null) {
+        this.tail = this.head;
+      }
+      break;
+    }
+    curr = curr.next;
+  }
 };
