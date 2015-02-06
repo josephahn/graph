02df438fe1ab335d431d11acdcb50c6c9847dccd
Joseph Ahn <joseph.t.ahn@gmail.com>
2015-02-06 15:41:54 -0500

    add skeleton constructor and prototype functions

---
+++ graph.js
@@ -0,0 +1,37 @@
+var Graph = function() {
+
+};
+
+Graph.prototype.adjacent = function(node1, node2) {
+
+};
+
+Graph.prototype.neighbors = function(node) {
+
+};
+
+Graph.prototype.add = function(node1, node2) {
+  
+};
+
+Graph.prototype.delete = function(node1, node2) {
+
+};
+
+Graph.prototype.getNodeValue = function(node) {
+
+};
+
+Graph.prototype.setNodeValue = function(node, val) {
+
+};
+
+/*
+Graph.prototype.getEdgeValue = function(node1, node2) {
+
+};
+
+Graph.prototype.setEdgeValue = function(node1, node2, val) {
+
+};
+*/
