73bb82b0700c43322a57e047b6a6d8f001cedbfb
Joseph Ahn <joseph.t.ahn@gmail.com>
2015-02-10 15:36:51 -0500

    add beforeEach hook

--- test/test.js
+++ test/test.js
@@ -5,9 +5,15 @@ var Vertex       = require('../src/vertex');
 var LinkedList   = require('../src/linkedList');
 
 describe('Graph', function() {
+  var g;
+
+  beforeEach(function() {
+    g = new Graph();
+  })
+
   describe('constructor', function() {
     it('should create new graph with empty adjList', function() {
-      var g = new Graph();
+      // var g = new Graph();
       assert.deepEqual(g.adjList, {});
     })
   })
@@ -16,7 +22,7 @@ describe('Graph', function() {
     it('should add edge between two verticies', function() {
       var v1 = new Vertex('a');
       var v2 = new Vertex('b');
-      var g = new Graph();
+      // var g = new Graph();
       g.add(v1, v2);
       assert.equal(g.adjList.a.head.val, 'b');
       assert.equal(g.adjList.b.head.val, 'a');
