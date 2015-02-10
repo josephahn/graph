2abfd5e90bd7776b294ce6863269e17ab9c909ed
Joseph Ahn <joseph.t.ahn@gmail.com>
2015-02-10 11:41:31 -0500

    add Graph add and print methods

--- graph.js
+++ graph.js
@@ -1,5 +1,5 @@
 var Graph = function() {
-
+  this.adjList = {};
 };
 
 Graph.prototype.adjacent = function(vert1, vert2) {
@@ -11,7 +11,20 @@ Graph.prototype.neighbors = function(vert) {
 };
 
 Graph.prototype.add = function(vert1, vert2) {
+  // TODO: don't add if list already contains value
+  var node1 = new Node(vert1.val);
+  var node2 = new Node(vert2.val);
+  if (!this.adjList.hasOwnProperty(vert1.val)) {
+    var linkedList = new LinkedList();
+    this.adjList[vert1.val] = linkedList;
+  }
+  this.adjList[vert1.val].add(node2);
 
+  if (!this.adjList.hasOwnProperty(vert2.val)) {
+    var linkedList = new LinkedList();
+    this.adjList[vert2.val] = linkedList;
+  }
+  this.adjList[vert2.val].add(node1);
 };
 
 Graph.prototype.delete = function(vert1, vert2) {
@@ -24,6 +37,19 @@ Graph.prototype.getVertValue = function(vert) {
 
 Graph.prototype.setVertValue = function(vert, val) {
 
+};
+
+Graph.prototype.print = function() {
+  for (var headVal in this.adjList) {
+    var result = headVal + ' -> ';
+    var node = this.adjList[headVal].head;
+    while (node !== null && node.hasOwnProperty('next')) {
+      result += node.val + ' -> ';
+      node = node.next;
+    }
+    result += 'NULL';
+    console.log(result);
+  }
 };
 
 /*
