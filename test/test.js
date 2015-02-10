4200a4ab647920d367708f88945d1b14a442c246
Joseph Ahn <joseph.t.ahn@gmail.com>
2015-02-10 17:20:24 -0500

    add tests for LinkedList

--- test/test.js
+++ test/test.js
@@ -4,16 +4,40 @@ var Node         = require('../src/node');
 var Vertex       = require('../src/vertex');
 var LinkedList   = require('../src/linkedList');
 
-describe('Graph', function() {
-  var g;
+describe('LinkedList', function() {
+  var linkedList;
 
   beforeEach(function() {
-    g = new Graph();
+    linkedList = new LinkedList();
+  })
+
+  describe('#add', function() {
+    it('should correctly set head and tail for one node', function() {
+      linkedList.add('a');
+      assert.equal(linkedList.head.val, 'a');
+      assert.equal(linkedList.tail.val, 'a');
+    })
+    it('should correctly set head and tail for multiple nodes', function() {
+      linkedList.add('a');
+      linkedList.add('b');
+      linkedList.add('c');
+      assert.equal(linkedList.head.val, 'a');
+      assert.equal(linkedList.head.next.val, 'b');
+      assert.equal(linkedList.tail.val, 'c');
+    })
+  })
+});
+
+describe('Graph', function() {
+  var graph;
+
+  beforeEach(function() {
+    graph = new Graph();
   })
 
   describe('constructor', function() {
     it('should create new graph with empty adjList', function() {
-      assert.deepEqual(g.adjList, {});
+      assert.deepEqual(graph.adjList, {});
     })
   })
 
@@ -21,15 +45,15 @@ describe('Graph', function() {
     var v1 = new Vertex('a');
     var v2 = new Vertex('b');
     it('should add edge between two verticies', function() {
-      g.add(v1, v2);
-      assert.equal(g.adjList.a.head.next.val, 'b');
-      assert.equal(g.adjList.b.head.next.val, 'a');
+      graph.add(v1, v2);
+      assert.equal(graph.adjList.a.head.next.val, 'b');
+      assert.equal(graph.adjList.b.head.next.val, 'a');
     })
     it('should only add unique edges', function() {
-      g.add(v1, v2);
-      g.add(v2, v1);
-      assert.equal(g.adjList.a.length(), 2);
-      assert.equal(g.adjList.b.length(), 2);
+      graph.add(v1, v2);
+      graph.add(v2, v1);
+      assert.equal(graph.adjList.a.length(), 2);
+      assert.equal(graph.adjList.b.length(), 2);
     })
   })
 });
