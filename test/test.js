ba64b247a9c63a55403ec9dbbde67c87faf10cd7
Joseph Ahn <joseph.t.ahn@gmail.com>
2015-02-13 21:02:47 -0500

    add tests for LinkedList methods remove, length and contains

--- test/test.js
+++ test/test.js
@@ -26,6 +26,63 @@ describe('LinkedList', function() {
       assert.equal(linkedList.tail.val, 'c');
     })
   })
+
+  describe('#remove', function() {
+    it('should handle a single node', function() {
+      linkedList.add('a');
+      linkedList.remove('a');
+      assert.equal(linkedList.head, null);
+      assert.equal(linkedList.tail, null);
+    })
+    it('should remove multiple nodes', function() {
+      linkedList.add('a');
+      linkedList.add('b');
+      linkedList.add('c');
+      linkedList.remove('b');
+      assert.equal(linkedList.head.next.val, 'c');
+      linkedList.remove('c');
+      assert.equal(linkedList.tail.val, 'a');
+    })
+  })
+
+  describe('#length', function() {
+    it('should return length of 0 for empty linked list', function() {
+      assert.equal(linkedList.length(), 0);
+    })
+    it('should return length of 1 for single node linked list', function() {
+      linkedList.add('a');
+      assert.equal(linkedList.length(), 1);
+    })
+    it('should return correct length for multiple nodes', function() {
+      linkedList.add('a');
+      linkedList.add('b');
+      linkedList.add('c');
+      assert.equal(linkedList.length(), 3);
+      linkedList.remove('a');
+      assert.equal(linkedList.length(), 2);
+      linkedList.remove('b');
+      linkedList.remove('c');
+      assert.equal(linkedList.length(), 0);
+    })
+  })
+
+  describe('#contains', function() {
+    it('should return true for nodes added to linked list', function() {
+      linkedList.add('a');
+      linkedList.add('b');
+      linkedList.add('c');
+      assert.equal(linkedList.contains('a'), true);
+      assert.equal(linkedList.contains('b'), true);
+      assert.equal(linkedList.contains('c'), true);
+    })
+    it('should return false for nodes not in linked list', function() {
+      linkedList.add('a');
+      linkedList.add('b');
+      linkedList.add('c');
+      assert.equal(linkedList.contains(1), false);
+      assert.equal(linkedList.contains('d'), false);
+    })
+  })
 });
 
 describe('Graph', function() {
